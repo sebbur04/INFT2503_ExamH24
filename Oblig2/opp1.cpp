a9d21f5701b702dd27df296c6c3a570469adee7a
Sebastian Burmo <sebaseb@stud.ntnu.no>
2025-09-01 15:17:47 +0200

    Opp 1 ferdig, kan ha noen bedre forklaringer

--- Oblig2/opp1.cpp
+++ Oblig2/opp1.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // Oppgave 1 - DEL A
-int main(void)
+int main()
 {
     //Kopierer følgende verdier fra øvingen
     int i = 3;
@@ -12,12 +12,28 @@ int main(void)
     int *p = &i; //pekeren P peker på adressen til i
     int *q = &j; //pekeren q peker på adressen til j
 
-    cout << "i: Addresse: " << &i << " Verdi: " << i << endl; //Addressen peker til verdien 3 i i
-    cout << "j: Addresse: " << &j << " Verdi: " << j << endl; //Addressen peker til verdien 5 i j
-    cout << "p: Addresse: " << p << " Verdi: " << *p << endl; //Pekkeren p referer til adressen til i, og verdien 3
-    cout << "q: Addresse: " << q << " Verdi: " << *q << endl; //Pekkeren q referer til adressen til j, og verdien 5
+    cout << "i: Addresse: " << &i << " Innholder følgende verdi: " << i << endl; //Addressen peker til verdien 3 i i
+    cout << "j: Addresse: " << &j << " Innholder følgende verdi: " << j << endl; //Addressen peker til verdien 5 i j
+    cout << "p: Addresse: " << p << " Innholder følgende verdi: " << *p << endl; //Pekkeren p referer til adressen til i, og verdien 3
+    cout << "q: Addresse: " << q << " Innholder følgende verdi: " << *q << endl; //Pekkeren q referer til adressen til j, og verdien 5
 
 // Oppgave 1 - DEL B (Utvidelse av kode)
+    cout << "-------- Del B -----------" << endl;
+    /*
+Koden som er skissert i oppgave 1b skal gjøre følgende:
+*p = 7;      // Setter verdien på adressen p peker til (i)
+*q += 4;   // Legger til 4 på verdien på adressen q peker til (j)
+*q = *p + 1; // Setter verdien på adressen q peker til (j) til verdien på adressen p peker til (i) + 1
+p = q;      // Setter pekeren p til å peke på samme adresse som q (j)
+//--> Nå vil begge pekerene peker på j som nå er 8
 
+*/
+
+    *p = 7;
+    *q += 4;
+    *q = *p + 1;
+    p = q;
+    cout << *p << " " << *q << endl; //(Begge peker til j som er 8)
+    //Utskrift: 8 8
     return 0;
 }
