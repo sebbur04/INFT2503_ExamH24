e570d5a1dce323444491dcfea2313efc9152eff5
Sebastian Burmo <sebaseb@stud.ntnu.no>
2025-09-25 23:15:09 +0200

    OBLIG 4 startet, OPP 1 er ferdig, men har problemmer med OPP2

--- Oblig4/opp1.cpp
+++ Oblig4/opp1.cpp
@@ -1,50 +1,25 @@
-//
-// Created by Sebastian E Burmo on 21/08/2025.
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-// Oppgave 1 - Rett opp feilene i følgende klasse:
+int main() {
+    cout << "Oppgave 1" << endl;
 
-const double pi = 3.141592;
+    vector<int> vec{1,2, 3, 4, 5};
 
-//Selve objekten skal ikke ligge i main, men blir kallet opp i main, for å verfisere at det fungerer
+    cout << "Første element: " << vec.front() << endl;
+    cout << "Siste element: " << vec.back() << endl;
 
-    class Circle {
+    vec.emplace(vec.begin() + 1, 6);
 
-    public:
-        Circle(double radius_);
-        int get_area() const;
-        double get_circumference() const;
+    cout << "Etter emplace, første element: " << vec.front() << endl;
 
-    private:
-        double radius;
-    };
-
-    // ==> Implementasjon av klassen Circle
-
-    Circle::Circle(double radius_) : radius(radius_) {} //Skal ikke være public, skrivefeil i radius
-
-    int Circle::get_area() const {
-        return pi * radius * radius;
+    auto it = find(vec.begin(), vec.end(), 6.6);
+    if (it != vec.end()) {
+        cout << "Fant verdi: " << *it << endl;
     }
 
-    double Circle::get_circumference() const {//Må legge til double
-
-        double circumference = 2.0 * pi * radius; //Må legge til double
-        return circumference;
-
-    }
-
-
-//Bruker en main og bruker klassen for å sjekke om objektene ovenfor fungerer som de skal
-// Selve konstruktøren skal ikke ligge i main
- int main () {
-
-        Circle circle (10); //Definere radius i sirklen
-
-        double circumference = circle.get_circumference(); // Finne omkretsen
-
-        cout << "Ved a kalle opp circumference far vi opp omkretsen til sirkel med radius pa 10cm" << circumference << endl;
-
-     return 0;
- }
+    return 0;
+}
--- Oblig4/opp2.cpp
+++ Oblig4/opp2.cpp
@@ -1,44 +1,84 @@
-//
-// Created by Sebastian E Burmo on 21/08/2025.
-#include <iostream>
-using namespace std;
+#include <gtkmm.h>
 
-//Kode fra oppgave 1, for å kjøre det i Oppgave 2
-const double pi = 3.141592;
-class Circle {
-
+class NameWindow : public Gtk::Window {
 public:
-    Circle(double radius_);
-    int get_area() const;
-    double get_circumference() const;
+    Gtk::Box main_box;
+    Gtk::Box name_box;
+    Gtk::Label first_name_label;
+    Gtk::Entry first_name_entry;
+    Gtk::Label last_name_label;
+    Gtk::Entry last_name_entry;
+    Gtk::Button combine_button;
+    Gtk::Label result_label;
+
+    NameWindow() : main_box(Gtk::ORIENTATION_VERTICAL, 10),
+                   name_box(Gtk::ORIENTATION_HORIZONTAL, 10) {
+
+        // Set window properties
+        set_title("Name Combiner");
+        set_default_size(400, 200);
+        set_border_width(10);
+
+        // Set up labels
+        first_name_label.set_text("First name:");
+        last_name_label.set_text("Last name:");
+
+        // Set up button
+        combine_button.set_label("Combine names");
+        combine_button.set_sensitive(false); // Initially disabled
+
+        // Set up result label
+        result_label.set_text("");
+
+        // Pack first name widgets
+        Gtk::Box* first_name_box = Gtk::make_managed<Gtk::Box>(Gtk::ORIENTATION_HORIZONTAL, 5);
+        first_name_box->pack_start(first_name_label, false, false);
+        first_name_box->pack_start(first_name_entry, true, true);
+
+        // Pack last name widgets
+        Gtk::Box* last_name_box = Gtk::make_managed<Gtk::Box>(Gtk::ORIENTATION_HORIZONTAL, 5);
+        last_name_box->pack_start(last_name_label, false, false);
+        last_name_box->pack_start(last_name_entry, true, true);
+
+        // Add everything to main box
+        main_box.pack_start(*first_name_box, false, false);
+        main_box.pack_start(*last_name_box, false, false);
+        main_box.pack_start(combine_button, false, false);
+        main_box.pack_start(result_label, false, false);
+
+        add(main_box);
+        show_all();
+
+        // Connect signals
+        first_name_entry.signal_changed().connect(
+            sigc::mem_fun(*this, &NameWindow::on_entry_changed));
+        last_name_entry.signal_changed().connect(
+            sigc::mem_fun(*this, &NameWindow::on_entry_changed));
+        combine_button.signal_clicked().connect(
+            sigc::mem_fun(*this, &NameWindow::on_combine_clicked));
+    }
 
 private:
-    double radius;
+    void on_entry_changed() {
+        // Enable button only if both entries have text
+        bool both_filled = !first_name_entry.get_text().empty() &&
+                          !last_name_entry.get_text().empty();
+        combine_button.set_sensitive(both_filled);
+
+        // Clear result when entries change
+        result_label.set_text("");
+    }
+
+    void on_combine_clicked() {
+        std::string first_name = first_name_entry.get_text();
+        std::string last_name = last_name_entry.get_text();
+        std::string full_name = first_name + " " + last_name;
+        result_label.set_text("Full name: " + full_name);
+    }
 };
 
-Circle::Circle(double radius_) : radius(radius_) {}
-
-int Circle::get_area() const {
-    return pi * radius * radius;
-}
-
-double Circle::get_circumference() const {
-
-    double circumference = 2.0 * pi * radius;
-    return circumference;
-}
-
-//----------------------------------------------------------------//
-//   Selve oppgaven, da jeg har skilt ut opp1.cpp og opp2.cpp     //
-//----------------------------------------------------------------//
-//Oppgave 2 - Følgende klientprogram bruker klassen fra oppgave 1. Fyll ut det som mangler (merket med /*---*/):
-
 int main() {
-    Circle circle(5);
-
-    int area = circle.get_area();
-    cout << "Arealet er lik " << area << endl;
-
-    double circumference = circle.get_circumference();
-    cout << "Omkretsen er lik " << circumference << endl;
+    auto app = Gtk::Application::create("org.example.namecombiner");
+    NameWindow window;
+    return app->run(window);
 }
