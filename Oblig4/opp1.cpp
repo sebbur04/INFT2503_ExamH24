e0d3e9e635219ce986d2616f817570c895ee7f85
Sebastian Burmo <sebaseb@stud.ntnu.no>
2025-09-27 01:16:23 +0200

    OBLIG 4 COMPLETE, ready to hand in :) Fixed maaaaany bugs and changed logic on oop2.cpp

--- Oblig4/opp1.cpp
+++ Oblig4/opp1.cpp
@@ -11,20 +11,23 @@ using namespace std;
 
 int main() {
     cout << "Oppgave 1" << endl;
+    // Creata a vector with double with 5 default values in the vector
+    vector<double> vec{1,2,3,4,5};
 
-    vector<int> vec{1,2, 3, 4, 5};
-
+    // Use front() and back() to print the first and last element in the vector
     cout << "Første element: " << vec.front() << endl;
     cout << "Siste element: " << vec.back() << endl;
 
-    vec.emplace(vec.begin() + 1, 6);
+    vec.emplace(vec.begin() , 6); // Change value from  1 to 6 for the first value in the vector
 
-    cout << "Etter emplace, første element: " << vec.front() << endl;
+    cout << "Etter emplace, første element: " << vec.front() << endl; //Print the first value aftr emplace change of the vector
 
-    auto it = find(vec.begin(), vec.end(), 6.6);
+    // Searched for value 2 in the vector and print it if found, error if not
+    double value = 4; //Added a hardcoded entry for which value I would like to find
+    auto it = find(vec.begin(), vec.end(), value);
     if (it != vec.end()) {
         cout << "Fant verdi: " << *it << endl;
+    } else {
+        cout << "Fant ikke verdien: " << value << endl;
     }
-
-    return 0;
 }
--- Oblig4/opp2.cpp
+++ Oblig4/opp2.cpp
@@ -1,91 +1,67 @@
 //
 // Created by Sebastian E Burmo on 25/09/2025
 // Oppgave 2 - Kombinerte navn program
-// Source code: https://gitlab.com/ntnu-iini4003/gtkmm-example
+// Source code: https://gitlab.com/ntnu-iini4003/gtkmm-example , used as a kind of template
 
 #include <gtkmm.h>
 
 // Create a class with all widgets as members
-class NameWindow : public Gtk::Window {
+class Window : public Gtk::Window {
 public:
-    Gtk::Box main_box;
-    Gtk::Box name_box;
-    Gtk::Label first_name_label;
-    Gtk::Entry first_name_entry;
-    Gtk::Label last_name_label;
-    Gtk::Entry last_name_entry;
-    Gtk::Button combine_button;
-    Gtk::Label result_label;
+    Gtk::Box vbox{Gtk::ORIENTATION_VERTICAL, 5};
+    Gtk::Label first_name_label, last_name_label, label;
+    Gtk::Entry first_name_entry, last_name_entry;
+    Gtk::Button button{"Combine names"};
 
-    NameWindow() : main_box(Gtk::ORIENTATION_VERTICAL, 10),
-                   name_box(Gtk::ORIENTATION_HORIZONTAL, 10) {
-
-        // Set window properties with size and width
-        set_title("ØVING 4");
+    // Set window properties with size and width
+    Window() {
+        set_title("Øving 4");
         set_default_size(400, 200);
-        set_border_width(10);
-
         // Application Labels
-        first_name_label.set_text("First name:");
-        last_name_label.set_text("Last name:");
+        first_name_label.set_text("First name");
+        last_name_label.set_text("Last name");
+        button.set_sensitive(false);
 
-        // Button properties
-        combine_button.set_label("Combine names");
-        combine_button.set_sensitive(false); // Initially disabled
+        // Add widgets to the vertical box, using PACK_SHRINK to keep natural size
+        // Found it on the box.h docks
+        vbox.pack_start(first_name_label, Gtk::PACK_SHRINK);
+        vbox.pack_start(first_name_entry, Gtk::PACK_SHRINK);
+        vbox.pack_start(last_name_label, Gtk::PACK_SHRINK);
+        vbox.pack_start(last_name_entry, Gtk::PACK_SHRINK);
+        vbox.pack_start(button, Gtk::PACK_SHRINK);
+        vbox.pack_start(label, Gtk::PACK_SHRINK);
 
-        // Set up result label, empty string which fills with content when button pressed
-        result_label.set_text("");
-
-        // First name
-        Gtk::Box* first_name_box = Gtk::make_managed<Gtk::Box>(Gtk::ORIENTATION_HORIZONTAL, 5);
-        first_name_box->pack_start(first_name_label, false, false);
-        first_name_box->pack_start(first_name_entry, true, true);
-
-        // Last name
-        Gtk::Box* last_name_box = Gtk::make_managed<Gtk::Box>(Gtk::ORIENTATION_HORIZONTAL, 5);
-        last_name_box->pack_start(last_name_label, false, false);
-        last_name_box->pack_start(last_name_entry, true, true);
-
-        // Add everything to main box
-        main_box.pack_start(*first_name_box, false, false);
-        main_box.pack_start(*last_name_box, false, false);
-        main_box.pack_start(combine_button, false, false);
-        main_box.pack_start(result_label, false, false);
-
-        add(main_box);
+        // Show All make all widgets visible, while vbox is beeing part of window
+        add(vbox);
         show_all();
 
-        // Connect signals
-        first_name_entry.signal_changed().connect(
-            sigc::mem_fun(*this, &NameWindow::on_entry_changed));
-        last_name_entry.signal_changed().connect(
-            sigc::mem_fun(*this, &NameWindow::on_entry_changed));
-        combine_button.signal_clicked().connect(
-            sigc::mem_fun(*this, &NameWindow::on_combine_clicked));
-    }
+        //First name and last name entry must be filled to enable button
+        first_name_entry.signal_changed().connect([this]() {
+            button.set_sensitive(
+                !first_name_entry.get_text().empty() &&
+                !last_name_entry.get_text().empty()
+            );
+        });
 
-private:
-    void on_entry_changed() {
-        // Enable button only if both entries have text
-        bool both_filled = !first_name_entry.get_text().empty() &&
-                          !last_name_entry.get_text().empty();
-        combine_button.set_sensitive(both_filled);
+        last_name_entry.signal_changed().connect([this]() {
+            button.set_sensitive(
+                !first_name_entry.get_text().empty() &&
+                !last_name_entry.get_text().empty()
+            );
+        });
 
-        // Clear result if entries change
-        result_label.set_text("");
-    }
-
-    void on_combine_clicked() {
-        std::string first_name = first_name_entry.get_text();
-        std::string last_name = last_name_entry.get_text();
-        std::string full_name = first_name + " " + last_name;
-        result_label.set_text("Names Combined: " + full_name); // Show combined names as result in the empty string in resultlabel
+        //When the button is clicked, comine name and show it in the label with set_text
+        button.signal_clicked().connect([this]() {
+            label.set_text("Names combined: " +
+                first_name_entry.get_text() + " " +
+                last_name_entry.get_text());
+        });
     }
 };
 
-// The same main as the gtkmm example repo
-int main() {
-    auto app = Gtk::Application::create();
-    NameWindow window;
+// Same as the example code from the repo
+int main(int argc, char* argv[]) {
+    auto app = Gtk::Application::create(argc, argv);
+    Window window;
     return app->run(window);
 }
