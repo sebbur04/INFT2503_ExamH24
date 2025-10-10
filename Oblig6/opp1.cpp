4a2d8ce1f955939b8cc143dcf5a6c27a36b7cc86
Sebastian Burmo <sebaseb@stud.ntnu.no>
2025-10-11 00:32:58 +0200

    OBLIG 6 COMPLETE, added comment from changes from last OBL5.

--- Oblig6/opp1.cpp
+++ Oblig6/opp1.cpp
@@ -1,57 +1,37 @@
 //
 // Created by Sebastian E Burmo on 10/10/2025.
+// Based on opp2.cpp in OBL5 with changes like ChessBoardPrint class and lambda functions for callbacks
 //
-
-// Oppgave 1
-// Kildekode: https://eidheim.folk.ntnu.no/c++/5-liv-og-død-smarte-pekere-og-polymorfi/exercise.html
-
 
 #include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
 class ChessBoard {
 public:
-  enum class Color { WHITE,
-                     BLACK };
+  enum class Color { WHITE, BLACK };
 
   class Piece {
   public:
     Piece(Color color) : color(color) {}
     virtual ~Piece() {}
-
     Color color;
-    std::string color_string() const {
-      if (color == Color::WHITE)
-        return "white";
-      else
-        return "black";
+    string color_string() const {
+      return color == Color::WHITE ? "white" : "black";
     }
-
-    /// Return color and type of the chess piece
-    virtual std::string type() const = 0;
-
-    /// Returns true if the given chess piece move is valid
+    virtual string type() const = 0;
     virtual bool valid_move(int from_x, int from_y, int to_x, int to_y) const = 0;
   };
 
-  // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  // Fullfør implementasjonene der det står // missing implementations.
-  // King arver fra Piece
   class King : public Piece {
-    // missing implementations - Oppgave 1
-  public: // Må sette opp public class for King
+  public:
     King(Color color) : Piece(color){}
-
-    string type() const override {
-      return color_string(); + "king";
-    }
-
+    string type() const override { return color_string() + " king"; }
     bool valid_move(int from_x, int from_y, int to_x, int to_y) const override {
-      // Kan spare skriving ved å implementere dx og dy som variabler for absoluttverdien av forskjellen mellom to_x og from_x, og to_y og from_y
       int dx = abs(to_x - from_x);
       int dy = abs(to_y - from_y);
       return (dx <= 1 && dy <= 1 && (dx != 0 || dy != 0));
@@ -59,13 +39,9 @@ public:
   };
 
   class Knight : public Piece {
-    // missing implementations - Oppgave 1
-  public: // Må sette opp public class for Knight
+  public:
     Knight(Color color) : Piece(color) {}
-    string type() const override {
-      return color_string() + " knight";
-    }
-
+    string type() const override { return color_string() + " knight"; }
     bool valid_move(int from_x, int from_y, int to_x, int to_y) const override {
       int dx = abs(to_x - from_x);
       int dy = abs(to_y - from_y);
@@ -74,18 +50,23 @@ public:
   };
 
   ChessBoard() {
-    // Initialize the squares stored in 8 columns and 8 rows:
     squares.resize(8);
     for (auto &square_column : squares)
       square_column.resize(8);
   }
 
-  /// 8x8 squares occupied by 1 or 0 chess pieces
   vector<vector<unique_ptr<Piece>>> squares;
 
-  /// Move a chess piece if it is a valid move.
-  /// Does not test for check or checkmate.
-  bool move_piece(const std::string &from, const std::string &to) {
+  // Declaring callback objects in chesssboard class, where I can set lamda functions
+  function<void(const Piece&, const string&, const string&)> on_piece_move;
+  function<void(const Piece&, const string&)> on_piece_removed;
+  function<void(const Piece&, const string&, const string&)> on_piece_move_invalid;
+  function<void(const string&)> on_piece_move_missing;
+  function<void(const King&)> on_lost_game;
+  function<void()> after_piece_move;
+
+  // Add callbacks, change from the previous code in OBL5
+  bool move_piece(const string &from, const string &to) {
     int from_x = from[0] - 'a';
     int from_y = stoi(string() + from[1]) - 1;
     int to_x = to[0] - 'a';
@@ -94,34 +75,91 @@ public:
     auto &piece_from = squares[from_x][from_y];
     if (piece_from) {
       if (piece_from->valid_move(from_x, from_y, to_x, to_y)) {
-        cout << piece_from->type() << " is moving from " << from << " to " << to << endl;
+        if (on_piece_move) on_piece_move(*piece_from, from, to);
         auto &piece_to = squares[to_x][to_y];
         if (piece_to) {
           if (piece_from->color != piece_to->color) {
-            cout << piece_to->type() << " is being removed from " << to << endl;
+            if (on_piece_removed) on_piece_removed(*piece_to, to);
             if (auto king = dynamic_cast<King *>(piece_to.get()))
-              cout << king->color_string() << " lost the game" << endl;
+              if (on_lost_game) on_lost_game(*king);
           } else {
-            // piece in the from square has the same color as the piece in the to square
-            cout << "can not move " << piece_from->type() << " from " << from << " to " << to << endl;
+            if (on_piece_move_invalid) on_piece_move_invalid(*piece_from, from, to);
             return false;
           }
         }
         piece_to = move(piece_from);
+        if (after_piece_move) after_piece_move();
         return true;
       } else {
-        cout << "can not move " << piece_from->type() << " from " << from << " to " << to << endl;
+        if (on_piece_move_invalid) on_piece_move_invalid(*piece_from, from, to);
         return false;
       }
     } else {
-      cout << "no piece at " << from << endl;
+      if (on_piece_move_missing) on_piece_move_missing(from);
       return false;
     }
+  }
+
+  void print(ostream &os) const {
+    for(int row = 7; row >= 0; row--) {
+      os << (row + 1) << " ";
+      for(int col = 0; col < 8; col++) {
+        auto &piece = squares[col][row];
+        if (piece) {
+          // Vis K+farge for king, N+farge for knight
+          string type_str = piece->type();
+          if (type_str.find("king") != string::npos) {
+            os << (piece->color == Color::WHITE ? "WK" : "BK");
+          } else if (type_str.find("knight") != string::npos) {
+            os << (piece->color == Color::WHITE ? "WN" : "BN");
+          }
+          os << " ";
+        } else {
+          os << " . "; // Tom rute uten brikke
+        }
+      }
+      os << "\n";
+    }
+    os << "  a  b  c  d  e  f  g  h "; // Rute bokstavene nederst på brettet
+  }
+};
+
+ostream &operator<<(ostream &os, const ChessBoard &board) {
+  board.print(os);
+  return os;
+}
+
+// definere funksjonsobjektene til ChessBoard instansen ved hjelp av lambda uttrykk.
+//Utskriften av sjakkbrettet etter et sjakktrekk skal også foregå i ChessBoardPrint
+class ChessBoardPrint {
+  ChessBoard &board;
+public:
+  ChessBoardPrint(ChessBoard &b) : board(b) {
+    board.on_piece_move = [](const ChessBoard::Piece &piece, const string &from, const string &to) {
+      cout << piece.type() << " is moving from " << from << " to " << to << endl;
+    };
+    board.on_piece_removed = [](const ChessBoard::Piece &piece, const string &square) {
+      cout << piece.type() << " is being removed from " << square << endl;
+    };
+    board.on_piece_move_invalid = [](const ChessBoard::Piece &piece, const string &from, const string &to) {
+      cout << "can not move " << piece.type() << " from " << from << " to " << to << endl;
+    };
+    board.on_piece_move_missing = [](const string &square) {
+      cout << "no piece at " << square << endl;
+    };
+    board.on_lost_game = [](const ChessBoard::King &king) {
+      cout << king.color_string() << " lost the game" << endl;
+    };
+    board.after_piece_move = [this]() { this->print_board(); };
+  }
+  void print_board() {
+    cout << board << endl;
   }
 };
 
 int main() {
   ChessBoard board;
+  ChessBoardPrint print(board); // getting the ChessBoardPrint, where the print is handled in the class
 
   board.squares[4][0] = make_unique<ChessBoard::King>(ChessBoard::Color::WHITE);
   board.squares[1][0] = make_unique<ChessBoard::Knight>(ChessBoard::Color::WHITE);
@@ -137,7 +175,14 @@ int main() {
   board.move_piece("b1", "b2");
   cout << endl;
 
+  cout << "Brikker" << endl;
+  cout << "BN equals a Black Knight" << endl;
+  cout << "BK equals a Black King" << endl;
+  cout << "WN equals a White Knight" << endl;
+  cout << "WK equals a White King" << endl;
+
   cout << "A simulated game:" << endl;
+  print.print_board();
   board.move_piece("e1", "e2");
   board.move_piece("g8", "h6");
   board.move_piece("b1", "c3");
--- Oblig6/opp2.cpp
+++ Oblig6/opp2.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Sebastian E Burmo on 10/10/2025.
 //
-
+// Based on: https://gitlab.com/ntnu-iini4003/examples6/-/blob/master/example6.cpp
 
 #include <boost/asio.hpp>
 #include <iostream>
@@ -22,35 +22,45 @@ private:
   tcp::endpoint endpoint;
   tcp::acceptor acceptor;
 
+  // Handles HTTP GET requests and sends valid HTTP responses
   void handle_request(shared_ptr<Connection> connection) {
     auto read_buffer = make_shared<boost::asio::streambuf>();
-    // Read from client until newline ("\r\n")
-    async_read_until(connection->socket, *read_buffer, "\r\n", [this, connection, read_buffer](const boost::system::error_code &ec, size_t) {
+    // Read until end of HTTP headers
+    async_read_until(connection->socket, *read_buffer, "\r\n\r\n", [this, connection, read_buffer](const boost::system::error_code &ec, size_t) {
       // If not error:
       if (!ec) {
         // Retrieve message from client as string:
         istream read_stream(read_buffer.get());
-        std::string message;
-        getline(read_stream, message);
-        message.pop_back(); // Remove "\r" at the end of message
+        string request_line;
+        getline(read_stream, request_line);
+        // Set statements on the HTTP website based on the string body, and create the full HTTP response for the page
+        string response;
 
-        // Close connection when "exit" is retrieved from client
-        if (message == "exit")
-          return;
+        // Case A - localhost:8080 -> main page --> "Dette er hovedsiden"
+        if (request_line.find("GET / HTTP/1.1") != string::npos) {
+          string body = "Dette er hovedsiden";
+          response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n" + body;
+        }
 
-        cout << "Message from a connected client: " << message << endl;
+        // Case B - localhost:8080/en_side --> "Dette er en side"
+        else if (request_line.find("GET /en_side HTTP/1.1") != string::npos) {
+          string body = "Dette er en side";
+          response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n" + body;
+        }
+
+        // Case C - localhost:8080/(Kan være hva som helst siden det er en else statement) --> "404 Not Found"
+        else {
+          string body = "404 Not Found";
+          response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: " + to_string(body.size()) + "\r\n\r\n" + body;
+        }
 
         auto write_buffer = make_shared<boost::asio::streambuf>();
         ostream write_stream(write_buffer.get());
+        write_stream << response;
 
-        // Add message to be written to client:
-        write_stream << message << "\r\n";
-
-        // Write to client
-        async_write(connection->socket, *write_buffer, [this, connection, write_buffer](const boost::system::error_code &ec, size_t) {
-          // If not error:
-          if (!ec)
-            handle_request(connection);
+        // Write HTTP response and close connection
+        async_write(connection->socket, *write_buffer, [connection, write_buffer](const boost::system::error_code &ec, size_t) {
+          // Connection closes after response
         });
       }
     });
@@ -58,10 +68,8 @@ private:
 
   void accept() {
     // The (client) connection is added to the lambda parameter and handle_request
-    // in order to keep the object alive for as long as it is needed.
+    // in order to keep the object alive for as long as it is needed
     auto connection = make_shared<Connection>(io_context);
-
-    // Accepts a new (client) connection. On connection, immediately start accepting a new connection
     acceptor.async_accept(connection->socket, [this, connection](const boost::system::error_code &ec) {
       accept();
       // If not error:
@@ -72,11 +80,11 @@ private:
   }
 
 public:
+  // Set the Echoserver to listen on port 8080, so you can connect with localhost 8080
   EchoServer() : endpoint(tcp::v4(), 8080), acceptor(io_context, endpoint) {}
 
   void start() {
     accept();
-
     io_context.run();
   }
 };
@@ -85,7 +93,8 @@ int main() {
   EchoServer echo_server;
 
   cout << "Starting echo server" << endl
-       << "Connect in a terminal with: telnet localhost 8080. Type 'exit' to end connection." << endl;
+       << "Connect in a terminal with: telnet localhost 8080. Type 'exit' to end connection"<< endl
+       << "For å teste oppgaven: Åpne i nettleser http://localhost:8080 eller http://localhost:8080/en_side eller et tilfeldig directory for 404 melding " << endl;
 
   echo_server.start();
 }
